83ba15e123f4a9673b603c2770b25c24035b5d31
paulo-santos-ds <pras.ds@outlook.com>
2025-01-28 23:29:14 -0300

    atualização de readme

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+#conexoes
 DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
 WHATSAPP_API_KEY = os.getenv('WHATSAPP_API_KEY')
 WHATSAPP_PHONE_NUMBER_ID = os.getenv('WHATSAPP_PHONE_NUMBER_ID')
